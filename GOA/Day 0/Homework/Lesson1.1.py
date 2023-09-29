1eb5eb4379827857ef3e8ba6dd4a3c0ff5c9b053
Luka <143455313+luviki@users.noreply.github.com>
2023-09-29 20:08:28 +0400

    Add files via upload

---
+++ GOA/Day 0/Homework/Lesson1.1.py
@@ -0,0 +1,49 @@
+#name = "Luka"
+
+#print (len(name)) #სიგრძის დაპრინტვა
+
+#my_sentence = "გამარჯობა, შეგახსენებთ რომ სწავლას იწყებთ 27 სექტემბერს"
+#თუ სტრინგი არის გრძელი თავში და ბოლოში ვუწერთ """ (3 ცალ) ბრჭყალს.
+
+#print(my_sentence)
+
+#knows_producing = True 
+
+#if 10>5:
+    #print("hello") #Tab-ის საშუალებით მოვახდინეთ ინდენტაცია(indentation)
+    #if არის ჩაშენებული დარეზერვებული keyword რასაც მოყვება space და შემდეგ მოყვება შესამოწმებელი პირობა
+    #მხოლოდ და მხოლოდ მაშინ გაეშვება თუ if-ის მერე ყველაფერი არის True ანუ ჭეშმარიტება
+
+    #TERMINAL-ის წაკითხვა უნდა ვიცოდეთ რადგან error-ები იქ ეწერება და უფრო მარტივად გავიგებთ ლოკაციას.
+
+full_name = "luka vishtekaliuki" #4+13=17 და ის 1 ადგილი არის SPACE-ის რადგან პროგრამაში SPACE-იც იკავებს ადგილს.
+#print(len(full_name))
+
+#ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად.
+#print(full_name[7])
+#position = index სინონიმებია
+
+#print("l" in full_name) 
+#print(full_name[2:7])
+#print(full_name[2:7:2]) #START:FINISH:STEP
+#print(full_name[:5])
+#print(full_name[5:])
+
+#print(full_name[-1:-6:-1])
+
+#სტრინგებს აქვთ მეთოდები
+# 1) სტრინგ.upper()
+# 2) len(სტრინგ)
+
+#print(full_name.upper()) #ეს გვიპრინტავს CAPSLOCK-ით
+
+name2 = " LUKA "
+# #print(name2.lower()) ეს აწ უკვე CAPSLOCk-ით დაწერილს დაპრინტავს პატარა შრიფტით
+# print(name2.lower()) 
+
+#print(name2.strip())
+
+#print(name2.replace(" ", "#")) --- ჩანაცვლება, პირველი "" ნიშნავს რას ვანაცვლებთ ხოლო მეორე "ამათ შორის" ვწერთ რითი ვანაცვლებთ.
+
+#print(name2.replace("U", "#")) --- ფენომენალური შესრულებაა საღოლ ლუკა XD
+
