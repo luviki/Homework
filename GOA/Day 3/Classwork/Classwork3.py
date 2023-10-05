ddf883068ab82c2aa0981216e149f4821f8f5059
Luka <143455313+luviki@users.noreply.github.com>
2023-10-05 10:21:09 +0400

    Add files via upload

---
+++ GOA/Day 3/Classwork/Classwork3.py
@@ -0,0 +1,70 @@
+# and the winner is...
+# Anna
+# 96
+name="Anna"
+score=96
+print(name)
+print(score)
+
+#flight Tracker
+# BA0117
+# New York
+# 1580
+
+#Storing the flight number
+flight_n = "BA0117"
+
+#Storing the flight information
+destination = "New York"
+distance = 1580
+
+print(flight_n)
+print(destination)
+print(distance)
+
+#It’s Payday!
+salary = 150000
+pay_raise = 10000
+
+# Create a variable named new_salary 
+
+new_salary=salary+pay_raise
+print(new_salary)
+
+#Chatbot v1.0
+
+# Ask the user for input and store it in a variable
+name = input()
+# Display the user input on the screen
+print(name)
+
+#Chatbot v1.1
+
+# Take the name as input
+name = input()
+
+# Use concatenation to join 2 strings
+message = "Nice to meet you, "
+
+# Display the message to the user
+print(message+name)
+
+#Module 2 Quiz
+message = "Debugging"
+print(message)
+band = "Beatles"
+song = "Yesterday"
+year = 1965
+
+score = 37
+score = 38
+#score = 15
+print(score)
+
+
+user1= "Tom"
+
+c= "moon"
+b= "guide"
+a= "honey"
+print(a+c)
---
+++ GOA/Day 3/Homework/Homework3.py
@@ -0,0 +1,9 @@
+name=input("your name ")
+surname=input("your surname ")
+age=input("your age ")
+print("your name is "+ name + " your surname is " + surname+" your age is " + age)
+
+your_age=int(input("your age "))
+new_age=your_age+25
+
+print("After 25 years you will be "+str(new_age) + "yo.")
