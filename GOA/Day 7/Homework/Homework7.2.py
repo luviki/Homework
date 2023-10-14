a69b40b3065e046fdf27b507988aaa1863e2b0dc
Luka <143455313+luviki@users.noreply.github.com>
2023-10-14 23:05:08 +0400

    Add files via upload

---
+++ GOA/Day 7/Homework/Homework7.2.py
@@ -0,0 +1,28 @@
+#დავალება 2: შექმენით თქვენი რაზმის წევრებისგან სია და დაპრინტეთ შუა 5 რაზმის წევრი(slicing)
+
+razmis_tsevrebi = [
+"Giorgi Lobzhanidze", 
+"Tinatin Zuzadze", 
+"Aleksandre Tordia", 
+"Beka Berashvili", 
+"Beqa Giorgobiani",
+"Davit Janashia",
+"Gio Abuladze",
+"Gio kacitadze",
+"Goglichadze Nini",
+"Guri Ko",
+"Lucas Vishtekaliuki",
+"Mirian Gelashvili",
+"Rati Murgulia",
+"Rezi Neparidze",
+"Salome Miladze",
+"Tekla Papava",
+"Temuri Solomonishvili",
+"Tsotne Sartania",
+"Vano Motiashvili",
+"Giorgi Chkhetiani",
+"Davit Tchilashvili",
+"Temo Labadze",
+"Nino Solomonia"
+]
+print(razmis_tsevrebi[8 : 13])
